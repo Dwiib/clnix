3d88471d76895f2bcdcabefa59b241c6b6e4f2be
Nathan Ringo <nathan@remexre.xyz>
2023-03-17 00:58:40 -0500

    Adds gefjon's immutable, which I'd like to keep building.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,5 @@
 {
   clnix-swank-server = lispPackages.callPackage ./clnix-swank-server { };
   coalton = lispPackages.callPackage ./coalton.nix { };
+  immutable = lispPackages.callPackage ./immutable.nix { };
 }
